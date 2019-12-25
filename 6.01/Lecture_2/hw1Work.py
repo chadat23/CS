858b359a23099f07dba9f52ff7634f6fa74bbff9
Chad DeRosier <chad.derosier@gmail.com>
2019-12-25 08:18:40 -0800

    HW 1 (for Lecture 2) working

--- 6.01/Lecture_2/hw1Work.py
+++ 6.01/Lecture_2/hw1Work.py
@@ -1,19 +1,50 @@
 import pdb
 # import lib601.sm as sm
+from copy import deepcopy
+
 from state_machine import SM
 import string
 import operator
 
+add_op = operator.add
+sub_op = operator.sub
+mul_op = operator.mul
+div_op = operator.truediv
+
 
 class BinaryOp:
+
     def __init__(self, left, right):
         self.left = left
         self.right = right
+        self.op = self.math_op
 
     def __str__(self):
         return self.opStr + '(' + \
                str(self.left) + ', ' + \
                str(self.right) + ')'
+
+    def eval(self, env):
+
+        op = deepcopy(self)
+        e = deepcopy(env)
+
+        if isinstance(op.left, BinaryOp) or isinstance(op.left, Number) or op.left.name in e:
+            left = op.left.eval(e)
+        else:
+            left = op.left
+
+        if isinstance(op.right, BinaryOp) or isinstance(op.right, Number) or op.right.name in e:
+            right = op.right.eval(e)
+        else:
+            right = op.right
+
+        if isinstance(left, float) and isinstance(right, float):
+            return op.op(left, right)
+        op.left = left
+        op.right = right
+
+        return op
 
     __repr__ = __str__
 
@@ -21,21 +52,33 @@ class BinaryOp:
 class Sum(BinaryOp):
     opStr = 'Sum'
 
+    math_op = add_op
+
 
 class Prod(BinaryOp):
     opStr = 'Prod'
+
+    math_op = mul_op
 
 
 class Quot(BinaryOp):
     opStr = 'Quot'
 
+    math_op = div_op
+
 
 class Diff(BinaryOp):
     opStr = 'Diff'
 
+    math_op = sub_op
+
 
 class Assign(BinaryOp):
     opStr = 'Assign'
+    math_op = None
+
+    def eval(self, env):
+        env[self.left.name] = self.right
 
 
 class Number:
@@ -44,6 +87,9 @@ class Number:
 
     def __str__(self):
         return 'Num(' + str(self.value) + ')'
+
+    def eval(self, env):
+        return self.value
 
     __repr__ = __str__
 
@@ -54,6 +100,14 @@ class Variable:
 
     def __str__(self):
         return 'Var(' + self.name + ')'
+
+    def eval(self, env):
+        var = env.get(self.name)
+        if var:
+            if not isinstance(var, Variable):
+                return var.eval(env)
+            return var
+        return self.name
 
     __repr__ = __str__
 
@@ -68,18 +122,17 @@ def tokenize(text):
         if text[i] == ' ':
             # a space was hit and should be skipped
             return j, None
-        elif text[i] in seps:
+        if text[i] in seps:
             # a special char was hit, should be noted and moved on from
             return j, text[i]
-        elif j == len(text):
+        if j == len(text):
             # the end of the text was hit, whatever's mid works is the last token
             return j, text[i: j]
-        elif text[j] not in seps and text[j] != ' ':
+        if text[j] not in seps and text[j] != ' ':
             # the next token char should be accounted for
             return tokenize_helper(text, i, j + 1)
-        else:
-            # the end of a token has been hit
-            return j, text[i: j]
+        # the end of a token has been hit
+        return j, text[i: j]
 
     tokens = []
     index = 0
@@ -96,8 +149,30 @@ def tokenize(text):
 # or one of the subclasses of {\tt BinaryOp} 
 def parse(tokens):
     def parseExp(index):
-        # <your code here>
-        pass
+        left_tree = tokens[index]
+        index += 1
+        if left_tree == '(':
+            left_tree, index = parseExp(index)
+        if numberTok(left_tree):
+            return Number(float(left_tree)), index
+        if variableTok(left_tree):
+            return Variable(left_tree), index
+        while tokens[index] == ')':
+            if index < len(tokens) - 1:
+                index += 1
+        op = tokens[index]
+        index += 1
+        right_tree, index = parseExp(index)
+        if op == '+':
+            return Sum(left_tree, right_tree), index
+        if op == '-':
+            return Diff(left_tree, right_tree), index
+        if op == '*':
+            return Prod(left_tree, right_tree), index
+        if op == '/':
+            return Quot(left_tree, right_tree), index
+        if op == '=':
+            return Assign(left_tree, right_tree), index
 
     (parsedExp, nextIndex) = parseExp(0)
     return parsedExp
@@ -106,16 +181,22 @@ def parse(tokens):
 # token is a string
 # returns True if contains only digits
 def numberTok(token):
+    if not hasattr(token, '__iter__'):
+        return False
     for char in token:
-        if not char in string.digits: return False
+        if char not in string.digits:
+            return False
     return True
 
 
 # token is a string
 # returns True its first character is a letter
 def variableTok(token):
+    if not hasattr(token, '__iter__'):
+        return False
     for char in token:
-        if char in string.letters: return True
+        if char in string.ascii_letters:
+            return True
     return False
 
 
@@ -130,7 +211,7 @@ def calc():
     env = {}
     while True:
         e = input('%')  # prints %, returns user input
-        print('%'),  # your expression here
+        print('%', parse(tokenize(e)).eval(env))  # your expression here
         print('   env ='), env
 
 
@@ -139,9 +220,9 @@ def calc():
 def calcTest(exprs):
     env = {}
     for e in exprs:
-        print('%'), e  # e is the experession
-        print()  # your expression here
-        print('   env ='), env
+        print('%', e)  # e is the experession
+        print(parse(tokenize(e)).eval(env))  # your expression here
+        print('   env =', env)
 
 
 # Simple tokenizer tests
@@ -167,8 +248,6 @@ def testTokenize():
     print(tokenize('(hi*ho)'))
     print(tokenize('( fred+george )'))
 
-
-testTokenize()
 
 # Simple parsing tests from the handout
 '''Answers are:
@@ -216,7 +295,41 @@ testExprs = ['(2 + 5)',
              '(w = (z + 1))',
              'w'
              ]
+
+
 # calcTest(testExprs)
+
+class Tokenizer(SM):
+    startState = ''
+
+    def getNextValues(self, state, inp):
+        if inp == " " or inp in seps or \
+                ((state in seps or state == ' ') and (inp in string.ascii_letters or inp in string.digits)):
+            return inp, state
+        return state + inp, ''
+
+
+def test_Tokenizer():
+    print(Tokenizer().transduce('fred '))
+    print(Tokenizer().transduce('777 '))
+    print(Tokenizer().transduce('777 hi 33 '))
+    print(Tokenizer().transduce('**-)( '))
+    print(Tokenizer().transduce('(hi*ho) '))
+    print(Tokenizer().transduce('(fred + george) '))
+
+
+def tokenize(inputString):
+    return [c for c in Tokenizer().transduce(inputString + ' ') if c != '' and c != ' ']
+
+
+def test_tokenize():
+    print(tokenize('fred'))
+    print(tokenize('777'))
+    print(tokenize('777 hi 33'))
+    print(tokenize('**-)('))
+    print(tokenize('(hi*ho)'))
+    print(tokenize('(fred + george) '))
+
 
 ####################################################################
 # Test cases for LAZY evaluator
@@ -263,4 +376,4 @@ partialTestExprs = ['(z = (y + w))',
                     '(w = 100)',
                     'z']
 
-# calcTest(partialTestExprs)
+calcTest(partialTestExprs)
